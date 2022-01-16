00f92be9e4e5cc94b4fcb9b8458444cd9fd20865
Rosivane Lopes <roselopes.net@gmail.com>
2022-01-16 07:41:33 -0300

    Capturar elementos htmal manipulaveis

---
+++ script.js
@@ -0,0 +1,5 @@
+const imagem = document.querySelector('img');
+const botao = document.querySelector('botton');
+const nomeDoPersonagem = document.querySelector('#nome');
+const especie = document.querySelector('#especie');
+const status = document.querySelector('#status');
